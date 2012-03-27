d0fc0924af74046534a216e5bb0190446ac3ccfc
Jay Janssen <jay.janssen@gmail.com>
2012-03-27 16:17:48 -0400

    added nodejs and rpmforge repos to puppet
    basic nodejs and AnyEvent modules installed

--- manifests/centos6.pp
+++ manifests/centos6.pp
@@ -9,4 +9,42 @@ file { '/etc/motd':
               Managed by Puppet.\n"
 }
 
+yumrepo {
+  'nodejs-stable':
+    descr       => "NodeJS Stable",
+    enabled     => 1,
+    baseurl     => absent,
+    mirrorlist  => "http://nodejs.tchol.org/mirrors/nodejs-stable-el\$releasever",
+    gpgcheck    => 0;
+    
+    
+  'rpmforge':
+    descr       => "RHEL \$releasever - RPMforge.net -dag",
+    enabled     => 1,
+    baseurl     => "http://apt.sw.be/redhat/el6/en/\$basearch/rpmforge",
+    mirrorlist  => "http://apt.sw.be/redhat/el6/en/mirrors-rpmforge",
+    gpgcheck    => 0;
+  'rpmforge-extras':
+    descr       => "RHEL \$releasever - RPMforge.net -extras",
+    enabled     => 1,
+    baseurl     => "http://apt.sw.be/redhat/el6/en/\$basearch/extras",
+    mirrorlist  => "http://apt.sw.be/redhat/el6/en/mirrors-rpmforge-extras",
+    gpgcheck    => 0;
+  'rpmforge-testing':
+    descr       => "RHEL \$releasever - RPMforge.net -testing",
+    enabled     => 1,
+    baseurl     => "http://apt.sw.be/redhat/el6/en/\$basearch/testing",
+    mirrorlist  => "http://apt.sw.be/redhat/el6/en/mirrors-rpmforge-testing",
+    gpgcheck    => 0;
+  
+}
 
+package {
+  'perl-AnyEvent': ensure => 'installed';
+  'perl-AnyEvent-HTTP': ensure => 'installed';
+
+
+  'nodejs-compat-symlinks': ensure => 'installed';
+  'npm': ensure => 'installed';
+
+}
