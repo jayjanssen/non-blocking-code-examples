ec1b8cf6f1bcc76671d7ce9f6a4537ac72bbe8cb
Jay Janssen <jay.janssen@gmail.com>
2012-04-08 15:39:56 -0400

    clean up formatting

--- code/timers.pl
+++ code/timers.pl
@@ -6,8 +6,13 @@ use AnyEvent;
 
 my $cv = AnyEvent->condvar;
 
-my $t = AnyEvent->timer( after => 5, cb => sub { print "5 seconds passed\n"; $cv->send; });
+my $t = AnyEvent->timer( after => 5, cb => sub { 
+    print "5 seconds passed\n"; 
+    $cv->send; 
+});
 
-my $t2 = AnyEvent->timer( after => 1, interval => 1, cb => sub {print "1 second passed\n"; });
+my $t2 = AnyEvent->timer( after => 1, interval => 1, cb => sub {
+    print "1 second passed\n"; 
+});
 
 $cv->wait;
