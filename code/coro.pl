f49801b6a0df7abea50a77f70b9ca0a7aeff5bb8
Jay Janssen <jay.janssen@gmail.com>
2012-04-09 11:18:55 -0700

    cleaned up coro example

--- code/coro.pl
+++ code/coro.pl
@@ -11,30 +11,13 @@ my $cv = AnyEvent->condvar;
 
 my $dbh = new AnyEvent::DBI "DBI:mysql:dbname=test", 'root', '';
 
-# Standard non-blocking call
-$dbh->exec ("select * from test where id=?", 10, sub {
-    my ($dbh, $rows, $rv) = @_;
-
-    $#_ or die "failure: $@";
-    
-    my $arr = $rows->[0];
-    print "(";
-    print join( ', ', @$arr );
-    print ")\n";
-
-    $cv->send;
-});
-
-$cv->wait;
-
-
-# non-blocking call to the DB.
+# non-blocking call to the DB, defer handling response until later.
 $dbh->exec( "select * from test where id=?", 10, Coro::rouse_cb );
 
-# do something else here, no nesting
+# do something else here
 
 # now block this execution path until the query results come back.
-my ($dbh, $rows, $rv) = Coro::rouse_wait;
+my ($dbh2, $rows, $rv) = Coro::rouse_wait;
 
 $#_ or die "failure: $@";
 
